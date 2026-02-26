d97645c9d18e7f6fec28bbcfe3521aa53874fe74
tasyatok <anastasiia.tokareva@outlook.com>
2026-02-26 14:53:22 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(650, 910);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  textSize(24);
+  textAlign(CENTER, CENTER);
+  text("Hello from your poster generator", width / 2, height / 2);
+}
